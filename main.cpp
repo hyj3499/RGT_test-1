124d2ace7c95a7b75335358b5fbcfe6090bf9c13
hyj3499 <qwer97363499@gmail.com>
2026-01-10 02:59:19 +0900

    feat: add to display error logs & exception handling

--- main.cpp
+++ main.cpp
@@ -15,6 +15,14 @@ int main() {
 
         std::vector<std::string> errorLogs = manager.readLogs("error.log");
 
+        for (size_t i = 0; i < errorLogs.size(); ++i) {
+            std::cout << "errorLogs[" << i << "] = \"" << errorLogs[i] << "\"" << std::endl;
+        }
+
+    } catch (const std::exception& e) {
+        // 예외 발생 시 처리
+        std::cerr << "런타임 에러: " << e.what() << std::endl;
+        return 1;
     } 
 
     return 0;
